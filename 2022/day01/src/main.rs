b7fc74800ed3c46d254a56cd4d512055f9e018dc
Tom Kaufman <me@tomkaufman.org>
2023-01-18 22:26:28 -0500

    naive solution implemented.

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -2,24 +2,37 @@ use std::error;
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 use std::process;
-use std::{thread, time};
 
-fn print_file_contents(input_path: &str) -> Result<(), Box<dyn error::Error>> {
+fn day1(input_path: &str) -> Result<usize, Box<dyn error::Error>> {
     let file = File::open(input_path)?;
     let reader = BufReader::new(file);
+    let mut this_cal = 0usize;
+    let mut this_elf = 0usize;
+    let mut max_cal = 0usize;
+    let mut max_elf = 0usize;
+    let mut line_str = String::new();
 
     for line in reader.lines() {
-        println!("{:?}", line?);
-        thread::sleep(time::Duration::from_millis(75));
+        line_str = line?;
+        if line_str.len() == 0 {
+            this_elf += 1;
+            if this_cal > max_cal {
+                max_cal = this_cal;
+                max_elf = this_elf;
+            }
+        } else {
+            this_cal += line_str.parse::<usize>()?;
+        }
     }
 
-    Ok(())
+    Ok(max_elf)
 }
 
 fn main() {
     let input_path = "input2.txt";
-    print_file_contents(input_path).unwrap_or_else(|err| {
-        println!("Problem printing file contents: {err}");
+    let which_elf = day1(input_path).unwrap_or_else(|err| {
+        println!("Problem during day1: {err}");
         process::exit(1);
     });
+    println!("The {which_elf}th elf has the most calories with them")
 }
