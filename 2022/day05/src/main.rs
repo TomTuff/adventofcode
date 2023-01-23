a0e291a67d6e415dc963b570a0e91ccc3d0c9022
Tom Kaufman <me@tomkaufman.org>
2023-01-22 21:49:56 -0500

    realized the test input and real input have different number of crate stacks, we should accomadate this

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -1,12 +1,16 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::error;
+use regex;
+
+//(\[[A-Z]\]\s|\s{4})
 
 #[derive(Default)]
 struct CrateStacks {
     // we have 9 crate stacks but we will use an array with length 10
     // so we don't have to do -1 on the indexing or any debugging
-    cratestacks: [Vec<String>; 10],  
+    cratestacks: Vec<Vec<String>>,
+    skip_lines: usize, // use this after we build it from a file to skip right to the proper line to pick up the command sequence
 }
 
 impl CrateStacks {
@@ -16,9 +20,18 @@ impl CrateStacks {
 
         let mut stack = CrateStacks::default();
 
+        let re = regex::Regex::new(r"(\[[A-Z]\]\s|\s{4})").unwrap();
+
         for line in reader.lines() {
             let line_str = line.expect("this should be a valid line 🤔");
+            println!("line {:?}: {line_str}", stack.skip_lines);
+            stack.skip_lines += 1;
             if line_str.len() == 0 { return Some(stack) }
+            
+            for (i, cap) in re.captures_iter(&line_str).enumerate() {
+                println!("capture {i}: {:?}", cap);
+            }
+            
         }
 
         None
