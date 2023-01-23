4515b705728fea0da23573665db7d97024ddb383
Tom Kaufman <me@tomkaufman.org>
2023-01-23 14:35:46 -0500

    now parses the initial crate order; bottom to top -> left to right

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -1,11 +1,10 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::error;
 use regex;
 
 //(\[[A-Z]\]\s|\s{4})
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 struct CrateStacks {
     stacks: Vec<Vec<String>>,
     num_crates: usize,
@@ -29,6 +28,7 @@ impl CrateStacks {
                     .trim()
                     .parse::<usize>()
                     .expect("regex guarantees this is a digit"));
+                
             }
         }        
 
@@ -37,6 +37,7 @@ impl CrateStacks {
         let file = File::open(file_path).expect("we should have this file 🤔");
         let reader = BufReader::new(file);
         let re_stack = regex::Regex::new(r"(\[[A-Z]\]\s?|\s{4})").unwrap();
+        let re_char = regex::Regex::new(r"[A-Z]").unwrap();
 
         for line in reader.lines() {
             let line_str = line.expect("this should be a valid line 🤔");
@@ -44,8 +45,15 @@ impl CrateStacks {
             
             for (i, cap) in re_stack.captures_iter(&line_str).enumerate() {
                 println!("capture {i}: {:?}", cap);
-            }
-            
+                if let Some(letter) = re_char.find(&cap[0]) {
+                    stack.stacks[i].insert(
+                        0, 
+                        letter
+                            .as_str()
+                            .to_string()
+                    )
+                }
+            }      
         }
 
         None
@@ -53,7 +61,8 @@ impl CrateStacks {
 
     fn set_num_crates(self: &mut Self, num_crates: usize) {
         self.num_crates = num_crates;
-        self.stacks = Vec::with_capacity(num_crates);
+        self.stacks.clear();
+        self.stacks.resize(num_crates, Default::default());
     }
 
     fn perform_sequence_from_file(self: &mut Self, file_path: &str) {}
@@ -68,6 +77,7 @@ fn main() {
     
     let mut stack = CrateStacks::from_file(input_file)
         .expect("puzzle input should guarantee CrateStacks");
+    println!("{:?}", stack);
     stack.perform_sequence_from_file(input_file);
     let top_crates = stack.top_sequence();
 
